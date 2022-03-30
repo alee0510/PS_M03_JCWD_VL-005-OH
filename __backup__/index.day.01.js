647ad3a5e1abbb1a6fd11c47062b4981f2a300a9
alee0510 <ali.muksin0510@gmail.com>
2022-03-30 14:30:35 +0700

    update new practice

---
+++ __backup__/index.day.01.js
@@ -0,0 +1,117 @@
+// import module
+const express = require('express')
+
+// initialize api
+const app =  express()
+
+// initialis middleware
+app.use(express.json())
+
+// database
+const products = [
+    { id : 1, name : 'Samsung Galaxy S22', price : 220000, brand : 'Samsung' },
+    { id : 2, name : 'Samsung Galaxy S21', price : 210000, brand : 'Samsung' },
+    { id : 3, name : 'iPhone 13 Pro', price : 250000, brand : 'Apple' }
+]
+
+// home route
+app.get('/', (request, respond) => {
+    respond.status(200).send(`<h1>Wellcome to my REST APIs</h1>`)
+})
+
+// CRUD (CREATE, READ, UPDATE, DELETE)
+// 1. READ
+app.get('/products', (req, res) => {
+    res.status(200).send(products)
+})
+
+// 1.1 READ single data => GET /products/:id => {...}
+app.get('/products/:id', (req, res) => {
+    const id = Number(req.params.id)
+    console.log('id' , id)
+
+    const data = products.filter((product) => product.id === id)
+
+    // vaidation
+    if (!data.length) {
+        res.status(404).send(`product with id : ${id} doesn't found.`)
+        return
+    }
+
+    res.status(200).send(data[0])
+})
+// 2. CREATE DATA => POST /products
+app.post('/products', (req, res) => {
+    let body = { ...req.body }
+    
+    body.id = products.length + 1
+    console.log(body)
+
+    // add new data products
+    products.push(body)
+
+    res.status(201).send(products)
+})
+// 3. UPDATE => PUT/PATCH /products/:id
+// PUT -> UPATE ENTIRE VALUE -> { name : 'ale', age : '19' } -> PUT ({ name : 'alfon' }) -> result => { name : 'alfon' }
+// PATCH -> PARTIAL UPDATE -> { name : 'ale', age : '19' } -> PATCH ({ name : 'alfon' }) -> result => { name : 'alfon', age : '19'}
+app.patch('/products/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const body = req.body
+    console.log('id', id)
+    console.log('body : ', req.body)
+
+    // check data
+    let index
+    for (let i = 0; i < products.length - 1; i++) {
+        if (id === products[i].id) {
+            index = i
+            break
+        }
+    }
+    console.log('index : ', index)
+    
+    // if failed
+    if (index === undefined) {
+        return res.status(400).send(`Bad Request : product with id : ${id} doesn't found.`)
+    }
+
+    // if exist -> do update
+    products[index] = Object.assign(products[index], body)
+
+    res.status(200).send(`product with id : ${id} has been updated.`)
+})
+
+// 4. DELETE => DELETE /products/:id
+app.delete('/products/:id', (req, res) => {
+    const id = Number(req.params.id)
+    console.log('id : ', id)
+
+    // check data
+    let index
+    for (let i = 0; i < products.length - 1; i++) {
+        if (id === products[i].id) {
+            index = i
+            break
+        }   
+    }
+
+    console.log(index)
+
+    if (index === undefined) {
+        res.status(404).send(`product with id : ${id} doesn't found.`)
+        return
+    }
+
+    // delete
+    products.splice(index, 1)
+
+    // send respond
+    res.status(200).send(`product with id : ${id} has been deleted.`)
+
+})
+
+
+// binding to our local port
+const PORT = 5000
+app.listen(PORT, () => console.log(`API run at port ${PORT}`))
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // import module
 const express = require('express')
+const fs = require('fs')
+const path = require('path')
 
 // initialize api
 const app =  express()
@@ -7,80 +9,65 @@ const app =  express()
 // initialis middleware
 app.use(express.json())
 
-// database
-const products = [
-    { id : 1, name : 'Samsung Galaxy S22', price : 220000, brand : 'Samsung' },
-    { id : 2, name : 'Samsung Galaxy S21', price : 210000, brand : 'Samsung' },
-    { id : 3, name : 'iPhone 13 Pro', price : 250000, brand : 'Apple' }
-]
 
 // home route
-app.get('/', (request, respond) => {
-    respond.status(200).send(`<h1>Wellcome to my REST APIs</h1>`)
+app.get('/', (req, res) => {
+    res.status(200).send(`<h1>Wellcome to my REST APIs</h1>`)
 })
 
-// CRUD (CREATE, READ, UPDATE, DELETE)
-// 1. READ
-app.get('/products', (req, res) => {
-    res.status(200).send(products)
+// get all clients
+app.get('/api/clients', (req, res) => {
+    fs.readFile(path.join(__dirname + '/clients.json'), (error, data) => {
+        // check error
+        if (error) {
+            console.log('error :', error)
+            return res.status(500).send('Internal Service Error.')
+        }
+        
+        // if success -> data exist
+        res.status(200).send(JSON.parse(data))
+    })
 })
 
-// 1.1 READ single data => GET /products/:id => {...}
-app.get('/products/:id', (req, res) => {
-    const id = Number(req.params.id)
-    console.log('id' , id)
+// get single data => /api/clients/:id
+app.get('/api/clients/:id', (req, res) => {
+    const id = req.params.id
+    console.log('id : ', id)
+    fs.readFile(path.join(__dirname + '/clients.json'), (error, data) => {
+        // check error
+        if (error) {
+            console.log('error :', error)
+            return res.status(500).send('Internal Service Error.')
+        }
 
-    const data = products.filter((product) => product.id === id)
+        // if success -> data exist -> filter data by request :id
+        const clients = JSON.parse(data)
+        let client
+        for (let i = 0; i < clients.length; i++) {
+            if (clients[i].id === id) {
+                client = clients[i]
+                break
+            }
+        }
 
-    // vaidation
-    if (!data.length) {
-        res.status(404).send(`product with id : ${id} doesn't found.`)
-        return
-    }
+        // check result
+        if (client === undefined) {
+            return res.status(400).send(`Bad Request : client with id : ${id} doesn't found.`)
 
-    res.status(200).send(data[0])
-})
-// 2. CREATE DATA => POST /products
-app.post('/products', (req, res) => {
-    let body = { ...req.body }
-    
-    body.id = products.length + 1
-    console.log(body)
+        }
 
-    // add new data products
-    products.push(body)
-
-    res.status(201).send(products)
-})
-// 3. UPDATE => PUT/PATCH /products/:id
-// 4. DELETE => DELETE /products/:id
-app.delete('/products/:id', (req, res) => {
-    const id = Number(req.params.id)
-    console.log(id)
-
-    // check data
-    let index
-    for (let i = 0; i < products.length - 1; i++) {
-        if (id === products[i].id) {
-            index = i
-        }   
-    }
-
-    console.log(index)
-
-    if (index === undefined) {
-        res.status(404).send(`product with id : ${id} doesn't found.`)
-        return
-    }
-
-    // delete
-    products.splice(index, 1)
-
-    // send respond
-    res.status(200).send(`product with id : ${id} has been deleted.`)
-
+        // send respond to client if result exist
+        res.status(200).send(client)
+    })
 })
 
+// post data => /api/clients
+// 1. get req.body
+// 2. create id => add to body, id => guid npm
+// 3. read file => JSON.parse(data) => [{...}, {...}, ...]
+// 4. push new data to result from JSON.parse
+// 5. convert new data => JSON.stringfy(new data)
+// 6. fs.writeFile
 
 // binding to our local port
 const PORT = 5000
