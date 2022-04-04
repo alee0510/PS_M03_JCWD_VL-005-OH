bb678b9489f7633b83449c3e9e660d4ccc3ef8e3
alee0510 <ali.muksin0510@gmail.com>
2022-04-04 15:00:48 +0700

    update sql file

--- __SQL_notes__/ps_notes_oh_04042022.sql
+++ __SQL_notes__/ps_notes_oh_04042022.sql
@@ -12,6 +12,15 @@ SELECT * FROM products;
 -- BASIC QUERY & FILTER
 SELECT * FROM customers WHERE country = 'USA';
 
+-- PRACTICES
 -- 1.1 GET customer data with credit limit more than 50000 and live in USA
+SELECT * FROM customers WHERE creditLimit >= 50000 AND country = 'USA';
+
 -- 1.2 GET top 10 customer with hight credit limit
--- 1.3 GET total customer for each region
+SELECT * FROM customers ORDER BY creditLimit DESC LIMIT 10;
+
+-- 1.3 GET total customer for each region/country
+SELECT country, COUNT(*) AS total_customers FROM customers GROUP BY country;
+
+-- 1.4 GET average credit limit of customers for each city and sort id by its country
+-- 1.5 GET the hight credit limit of customers for each city, show its name and sort by its country
