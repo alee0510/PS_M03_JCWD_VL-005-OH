723574ff345357d5420fe2849b8000a56aa20e35
alee0510 <ali.muksin0510@gmail.com>
2022-04-06 14:13:26 +0700

    update new sql notes

--- __SQL_notes__/ps_notes_oh_06042022.sql
+++ __SQL_notes__/ps_notes_oh_06042022.sql
@@ -16,3 +16,16 @@ ORDER by country ASC;
 -- 1.1 GET data employee, total customers, country, and average credit from employee and customers table
 -- 1.1.b do the samething, but join firstName and lastName of employee --> CONCAT()
 -- 1.1.c do the samething, join all customer name as list or one value --> GROUPCONCAT()
+SELECT em.employeeNumber,
+CONCAT(em.firstname,' ', em.lastName) employeeName, 
+COUNT(cs.customerName) total_customers , 
+GROUP_CONCAT(cs.customerName SEPARATOR ';') customers,
+AVG(cs.creditLimit) avg_credit, 
+cs.country
+FROM customers AS cs
+JOIN employees AS em
+ON cs.salesRepEmployeeNumber = em.employeeNumber
+GROUP BY em.employeeNumber
+ORDER BY cs.country;
+
+-- 1.2 GET data employee, office, job title, total customers, customer country & city
