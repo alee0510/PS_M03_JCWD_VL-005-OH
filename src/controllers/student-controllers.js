f75b67bd66309db325e83a5fc1d1537d5a27518e
alee0510 <ali.muksin0510@gmail.com>
2022-04-11 16:13:25 +0700

    update practices

--- src/controllers/student-controllers.js
+++ src/controllers/student-controllers.js
@@ -1,4 +1,5 @@
 const database = require('../config').promise()
+const http_status = require('../helpers/http-status')
 
 // callback
 // module.exports.getStudents = (req, res) => {
@@ -18,10 +19,17 @@ const database = require('../config').promise()
 
 // promise wrapper
 module.exports.getStudents = async (req, res) => {
-    try {
-        // define query
-        const GET_STUDENT = 'SELECT * FROM students;'
+    // check query params
+    const limit = req.query._limit ? req.query._limit : 5
+    const page = req.query._page ? req.query._page : 1
+    const offset = (page - 1 ) * limit
+    console.log(limit, page, offset)
 
+    // define query
+    let GET_STUDENT = `SELECT * FROM students LIMIT ${offset}, ${limit};`
+    console.log(`query : `, GET_STUDENT)
+
+    try {
         // execute query
         const [ STUDENTS ] = await database.execute(GET_STUDENT)
 
@@ -35,3 +43,24 @@ module.exports.getStudents = async (req, res) => {
 // GET : get student by ID
 // GET : get students -> do limitation or pagination, 
 // QUERY : _page ? & _limit ?, deafault limit = 5
+
+module.exports.getStudentById = async (req, res) => {
+    const student_id = req.params.student_id
+    const GET_STUDENT_BY_ID = `SELECT * FROM students WHERE student_id = ?;`
+    try {
+        const [ STUDENT ] = await database.execute(GET_STUDENT_BY_ID, [student_id])
+        console.log(STUDENT)
+
+        // validate
+        if (!STUDENT.length) {
+            throw { status : http_status.BAD_REQUEST, message : 'bad request.' }
+        } 
+
+        res.status(200).send(STUDENT[0])
+    } catch (error) {
+        console.log('error : ', error)
+        res.status(error.status || 500).send(error.message || 'Internal service error.')
+    }
+}
+
+// POST & PACTH, DELETE
--- src/routes/student-routers.js
+++ src/routes/student-routers.js
@@ -5,6 +5,7 @@ const { student_controllers } = require('../controllers')
 
 // define routers
 router.get('/students', student_controllers.getStudents)
+router.get('/students/:student_id', student_controllers.getStudentById)
 
 // export * router
 module.exports = router
